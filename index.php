42077b7e0f26423f11a27d0f3a645dafe0f51d07
Hali Velazquez <halivelazquez@outlook.com>
2017-10-19 16:14:00 -0500

    Try to php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sitio de Halivert</title>
+  </head>
+  <body>
+    <h1>Hola, soy Hali.</h1>
+
+    Este sitio está en construcción...
+  </body>
+</html>
