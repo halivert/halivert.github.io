ee123f705d6db08d3eb5fc715ac1d2a78e58317b
Hali Velazquez <halivelazquez@outlook.com>
2018-11-22 01:25:48 -0600

    Update, no need for jquery :)

--- js/master.js
+++ js/master.js
@@ -1,53 +1,71 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', function(event) {
+  let resizeEnd = new Event('resizeEnd');
+
   changeScreen();
-  $(window).resize(function() {
-    if(this.resizeTO) clearTimeout(this.resizeTO);
-    this.resizeTO = setTimeout(function() {
-      $(this).trigger('resizeEnd');
+
+  window.addEventListener('resize', function(event) {
+    if (window.resizeTO) {
+      clearTimeout(window.resizeTO);
+    }
+    window.resizeTO = setTimeout(function() {
+      window.dispatchEvent(resizeEnd);
     }, 50);
   });
 
   changeFooterPosition();
 
-  $(window).on('resizeEnd', changeScreen);
+  window.addEventListener('resizeEnd', changeScreen, false);
 });
 
 function changeFooterPosition() {
-  var commentsHeight = typeof hasComments !== 'undefined' ? (hasComments ? 400 : 0) : 0;
-  if ($(document).height() + commentsHeight > $(window).height()) {
-    $('#footer').css('position', 'relative');
+  let commentsHeight =
+    typeof hasComments !== 'undefined' ? (hasComments ? 400 : 0) : 0;
+
+  let bodyHeight = document.body.clientHeight;
+  let windowHeight = window.innerHeight;
+  let footer = document.getElementById('footer');
+
+  if (bodyHeight + commentsHeight > windowHeight) {
+    footer.style.position = 'relative';
   }
   else {
-    $('#footer').css('position', 'fixed');
+    footer.style.position = 'fixed';
   }
 }
 
 function changeScreen() {
   changeFooterPosition();
-  let width = $(window).width();
+  let width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
   let items;
 
-  $('.mobile-separator').attr('hidden', width >= 768);
+  document.querySelectorAll('.mobile-separator').forEach(function(it) {
+    it.style.display = width < 768 ? 'inline' : 'none';
+  });
+
   if (width < 768) {
-    $('.is-size-1').each(function() {
-      $(this).removeClass('is-size-1');
-      $(this).addClass('is-size-2-mobile');
+    document.querySelectorAll('.is-size-1').forEach(function(it) {
+      it.classList.remove('is-size-1');
+      it.classList.add('is-size-2-mobile');
     });
 
-    $('.is-size-5').each(function() {
-      $(this).removeClass('is-size-5');
-      $(this).addClass('is-size-5-mobile');
+    document.querySelectorAll('.is-size-5').forEach(function(it) {
+      it.classList.remove('is-size-5');
+      it.classList.add('is-size-5-mobile');
     });
   }
   else {
-    $('.is-size-2-mobile').each(function() {
-      $(this).removeClass('is-size-2-mobile');
-      $(this).addClass('is-size-1');
+    let size2mob = '.is-size-2-mobile';
+    document.querySelectorAll(size2mob).forEach(function(it) {
+      it.classList.remove('is-size-2-mobile');
+      it.classList.add('is-size-1');
     });
 
-    $('.is-size-5-mobile').each(function() {
-      $(this).addClass('is-size-5');
-      $(this).removeClass('is-size-5-mobile');
+    let size5mob = '.is-size-5-mobile';
+    document.querySelectorAll(size5mob).forEach(function(it) {
+      it.classList.remove('is-size-5');
+      it.classList.add('is-size-5-mobile');
     });
   }
 }
