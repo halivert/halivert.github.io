a5342719141d5913642fd734f75c307d10bb39df
Hali Velazquez <halivelazquez@outlook.com>
2019-02-04 00:39:14 -0600

    Update footer (final?)

--- js/master.js
+++ js/master.js
@@ -16,24 +16,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
 });
 
 function changeScreen() {
-  let windowHeight = window.innerHeight;
-  let footer = document.getElementById('footer');
-  let footerHeight = footer.clientHeight;
-  let mainContainer = document.getElementById('main-container');
-  let mainContainerHeight = mainContainer.clientHeight;
-  let bodyHeight = Math.max(document.body.clientHeight,
-    mainContainerHeight + footerHeight);
-
-  if (bodyHeight != document.body.clientHeight)
-    document.body.style.height = bodyHeight + 'px';
-
-  if (bodyHeight > windowHeight) footer.style.position = 'relative';
-  else {
-    bodyHeight = windowHeight - footerHeight;
-    document.body.style.height = bodyHeight + 'px';
-    footer.style.position = 'fixed';
-  }
-
   let width = window.innerWidth
     || document.documentElement.clientWidth
     || document.body.clientWidth;
