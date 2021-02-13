f98ad44e15addfd77bd0cac6fb682670493ef2f9
Halí V <halivelazquez@outlook.com>
2021-02-12 20:32:01 -0600

    Frameworkless site
    I use a lot of Bulma srry 🙏🏽.
    
    First approach columns
    
    Solve some issues
    
    Almost done
    
    Improve search modal
    
    Finish repo layout
    
    Finish repo layout
    
    Ugly light theme
    
    Update building page, update menu
    
    Lunr for everyone
    
    Prod files

--- _resources/js/main.js
+++ _resources/js/main.js
@@ -1,120 +1,132 @@
-const addClass = (element, name) => {
-	if (!element) return
+function addClass(element, name) {
+  if (!element) return
 
-	if (element.classList) {
-		element.classList.add(name)
-	} else {
-		let arr = element.className.split(" ")
-		if (arr.indexOf(name) === -1) element.className += " " + name
-	}
+  if (element.classList) {
+    element.classList.add(name)
+  } else {
+    let arr = element.className.split(" ")
+    if (arr.indexOf(name) === -1) element.className += " " + name
+  }
 }
 
-const toggleClass = (element, className) => {
-	if (element.classList) {
-		element.classList.toggle(className)
-	} else {
-		let classes = element.className.split(" ")
-		let i = classes.indexOf(className)
+function toggleClass(element, className) {
+  if (element.classList) {
+    element.classList.toggle(className)
+  } else {
+    let classes = element.className.split(" ")
+    let i = classes.indexOf(className)
 
-		if (i >= 0) {
-			classes.splice(i, 1)
-		} else {
-			classes.push(className)
-		}
+    if (i >= 0) {
+      classes.splice(i, 1)
+    } else {
+      classes.push(className)
+    }
 
-		element.className = classes.join(" ")
-	}
+    element.className = classes.join(" ")
+  }
 }
 
-const hasClass = (element, name) => {
-	if (!element) return
+function hasClass(element, name) {
+  if (!element) return
 
-	if (element.classList) {
-		return element.classList.contains(name)
-	} else {
-		let classes = element.className.split(" ")
-		let i = classes.indexOf(name)
-		return i >= 0
-	}
+  if (element.classList) {
+    return element.classList.contains(name)
+  } else {
+    let classes = element.className.split(" ")
+    let i = classes.indexOf(name)
+    return i >= 0
+  }
 }
 
-const removeClass = (element, name) => {
-	if (!element) return
+function removeClass(element, name) {
+  if (!element) return
 
-	if (element.classList) {
-		element.classList.remove(name)
-	} else {
-		let classes = element.className.split(" ")
-		let i = classes.indexOf(className)
+  if (element.classList) {
+    element.classList.remove(name)
+  } else {
+    let classes = element.className.split(" ")
+    let i = classes.indexOf(className)
 
-		if (i >= 0) classes.splice(i, 1)
-		element.className = classes.join(" ")
-	}
+    if (i >= 0) classes.splice(i, 1)
+    element.className = classes.join(" ")
+  }
 }
 
-const isInput = (name) => {
-	let compare = name.toLowerCase()
-	return ["input", "textarea", "select"].includes(compare)
+function isInput(name) {
+  let compare = name.toLowerCase()
+  return ["input", "textarea", "select"].includes(compare)
 }
 
-const vibrate = (pattern) => {
-	window.navigator.vibrate(pattern)
+function vibrate(pattern) {
+  window.navigator.vibrate(pattern)
 }
 
-const toggleTheme = (element, icon, isDark) => {
-	removeClass(icon, isDark ? "fa-moon" : "fa-sun")
-	addClass(icon, isDark ? "fa-sun" : "fa-moon")
-	removeClass(element, isDark ? "is-dark" : "is-warning")
-	addClass(element, isDark ? "is-warning" : "is-dark")
+function toggleTheme(element, icon, isDark) {
+  removeClass(icon, isDark ? "fa-moon" : "fa-sun")
+  addClass(icon, isDark ? "fa-sun" : "fa-moon")
+  removeClass(element, isDark ? "is-dark" : "is-warning")
+  addClass(element, isDark ? "is-warning" : "is-dark")
 }
 
-const initThemeSwitcher = (element) => {
-	if (!element) return
+function initThemeSwitcher(element) {
+  if (!element) return
 
-	const firstSpan = element.getElementsByTagName("span")[0]
-	const icon = firstSpan.getElementsByTagName("i")[0]
-	const isDark = hasClass(document.documentElement, "dark")
+  const firstSpan = element.getElementsByTagName("span")[0]
+  const icon = firstSpan.getElementsByTagName("i")[0]
+  const isDark = hasClass(document.documentElement, "dark")
 
-	toggleTheme(element, icon, isDark)
+  toggleTheme(element, icon, isDark)
 
-	removeClass(element, "is-invisible")
+  removeClass(element, "is-invisible")
 }
 
-const setTheme = () => {
-	if (localStorage.halivertsTheme && localStorage.halivertsTheme === "light") {
-		removeClass(document.documentElement, "dark")
-		localStorage.halivertsTheme = "light"
-	} else {
-		localStorage.removeItem("halivertsTheme")
-	}
+function setTheme() {
+  if (localStorage.halivertsTheme && localStorage.halivertsTheme === "light") {
+    removeClass(document.documentElement, "dark")
+    localStorage.halivertsTheme = "light"
+  } else {
+    localStorage.removeItem("halivertsTheme")
+  }
+}
+
+function hideModal(evt) {
+  if (!evt) return
+
+  const modal = evt.closest(".modal")
+  if (!modal) return
+
+  removeClass(document.documentElement, "is-clipped")
+  addClass(modal, "is-hidden")
 }
 
 setTheme()
 
 document.addEventListener("turbolinks:load", () => {
-	let searchBox = document.getElementById("search-box")
-	if (searchBox) {
-		document.addEventListener("keypress", (evt) => {
-			if (!isInput(evt.target.nodeName) && evt.charCode === 47) {
-				evt.preventDefault()
-				activateNavMenu()
-				searchBox.focus()
-				searchBox.select()
-			}
-		})
-	}
+  removeClass(document.documentElement, "is-clipped")
+  initThemeSwitcher(document.getElementById("theme-switcher"))
+  initThemeSwitcher(document.getElementById("theme-switcher-corner"))
 
-	initThemeSwitcher(document.getElementById("theme-switcher"))
-	initThemeSwitcher(document.getElementById("theme-switcher-corner"))
+  const deletes = document.querySelectorAll(".notification .delete") || []
 
-	const deletes = document.querySelectorAll(".notification .delete") || []
+  deletes.forEach((deleteElement) => {
+    notification = deleteElement.parentNode
+    deleteElement.addEventListener("click", () => {
+      notification.parentNode.removeChild(notification)
+    })
+  })
 
-	deletes.forEach((deleteElement) => {
-		notification = deleteElement.parentNode
-		deleteElement.addEventListener("click", () => {
-			notification.parentNode.removeChild(notification)
-		})
-	})
+  const modals = document.querySelectorAll(".modal") || []
+  modals.forEach((modal) => {
+    modal.addEventListener("click", (evt) => {
+      if (evt.target !== modal) {
+        return
+      }
+
+      evt.preventDefault();
+      toggleClass(modal, "is-hidden")
+      removeClass(document.documentElement, "is-clipped")
+    })
+  })
 })
 
 document.addEventListener("turbolinks:visit", setTheme)
--- _resources/js/search.js
+++ _resources/js/search.js
@@ -1,106 +1,107 @@
-document.addEventListener("turbolinks:load", () => {
-  function htmlPostElement(item, template) {
-    let newElement = template.content.cloneNode(true)
+function showSearchModal() {
+  const modal = document.getElementById("search-modal")
+  const input = document.getElementById("search-input")
 
-    let pictureEl = newElement.querySelector("picture.post-picture")
-    if (item.image_types && item.image) {
-      item.image_types.split(",").forEach((type, idx, arr) => {
-        let [ext, mime] = type.split(":"),
-          imgEl
-        if (idx === arr.length - 1) {
-          imgEl = pictureEl.appendChild(document.createElement("img"))
-          imgEl.src = `${item.image}.${ext}`
-          imgEl.alt = item.image_alt
-        } else {
-          imgEl = pictureEl.appendChild(document.createElement("source"))
-          imgEl.srcset = `${item.image}.${ext}`
-          imgEl.type = mime
-        }
-      })
-    } else {
-      pictureEl.remove()
-    }
+  removeClass(modal, "is-hidden")
+  addClass(document.documentElement, "is-clipped")
 
-    let titleEl = newElement.querySelector("a.post-title")
-    titleEl.href = item.url
-    titleEl.text = item.title
+  if (input) document.getElementById("search-input").focus()
+}
 
-    newElement.querySelector("span.post-author").innerText = item.author
-    newElement.querySelector("span.post-date").innerText = item.date
+function hideSearchModal() {
+  const modal = document.getElementById("search-modal")
+  const input = document.getElementById("search-input")
 
-    let contentEl = newElement.querySelector("div.post-content")
-    contentEl.innerHTML = item.content
+  addClass(modal, "is-hidden")
 
-    if (item.continue === 1) {
-      contentEl.appendChild(document.createElement("p")).innerHTML = "&#8230;"
+  if (input) input.value = ""
 
-      let keepReadingEl = contentEl.appendChild(document.createElement("a"))
-      keepReadingEl.classList.add("subtitle", "is-5", "has-text-link")
-      keepReadingEl.href = item.url
-      keepReadingEl.text = "Seguir leyendo"
-    }
+  displaySearchResults([], window.store)
+  removeClass(document.documentElement, "is-clipped")
+}
 
-    return newElement
+function inputSearch(input) {
+  if (!input || !idx) return
+
+  const searchTerm = input.value
+
+  let results = searchTerm ? idx.search(searchTerm) : []
+  displaySearchResults(results, window.store)
+}
+
+function displaySearchResults(results, store) {
+  let searchResults = document.getElementById("search-results")
+  let template = document.getElementById("post-template")
+  if (!searchResults) return
+
+  while (searchResults.firstChild) {
+    searchResults.removeChild(searchResults.lastChild)
   }
 
-  function displaySearchResults(results, store) {
-    let searchResults = document.getElementById("search-results")
-    let template = document.getElementById("post-template")
-    if (!searchResults) return
+  if (results.length) {
+    for (let i = 0; i < results.length; i++) {
+      let item = store[results[i].ref]
+      searchResults.appendChild(htmlPostElement(item, template))
+    }
+  }
+}
 
-    if (results.length) {
-      for (let i = 0; i < results.length; i++) {
-        let item = store[results[i].ref]
-        searchResults.appendChild(htmlPostElement(item, template))
+function htmlPostElement(item, template) {
+  let newElement = template.content.cloneNode(true)
+
+  let pictureEl = newElement.querySelector("picture.post-picture")
+  if (item.image_types && item.image) {
+    item.image_types.split(",").forEach((type, idx, arr) => {
+      let [ext, mime] = type.split(":"),
+        imgEl
+      if (idx === arr.length - 1) {
+        imgEl = pictureEl.appendChild(document.createElement("img"))
+        imgEl.src = `${item.image}.${ext}`
+        imgEl.alt = item.image_alt
+      } else {
+        imgEl = pictureEl.appendChild(document.createElement("source"))
+        imgEl.srcset = `${item.image}.${ext}`
+        imgEl.type = mime
       }
-    } else {
-      let nothingEl = searchResults.appendChild(document.createElement("p"))
-      nothingEl.classList.add("title", "is-3")
-      nothingEl.appendChild(document.createTextNode("No encontré nada :("))
-      nothingEl.appendChild(document.createElement("br"))
-      nothingEl.appendChild(
-        document.createTextNode("Pero quizá esto pueda interesarte")
-      )
-
-      let template = document.getElementById("post-template")
-      searchResults.appendChild(htmlPostElement(getRandomPost(), template))
-    }
-  }
-
-  function getQueryVariable(variable) {
-    let query = window.location.search.substring(1)
-    let vars = query.split("&")
-
-    for (let i = 0; i < vars.length; i++) {
-      let pair = vars[i].split("=")
-
-      if (pair[0] === variable)
-        return decodeURIComponent(pair[1].replace(/\+/g, "%20"))
-    }
-  }
-
-  function getRandomPost() {
-    let keys = Object.keys(window.store)
-    let i = Math.floor(Math.random() * keys.length)
-    return window.store[keys[i]]
-  }
-
-  let searchTerm = getQueryVariable("consulta")
-
-  if (searchTerm) {
-    document.getElementById("search-box").setAttribute("value", searchTerm)
-    let results = idx.search(searchTerm)
-    displaySearchResults(results, window.store)
+    })
   } else {
-    let searchResults = document.getElementById("search-results")
-    if (!searchResults) return
-
-    let textEl = searchResults.appendChild(document.createElement("p"))
-    textEl.classList.add("title", "is-3")
-    textEl.innerText =
-      "Si no sabes que buscar, puedes comenzar con este post aleatorio"
-
-    let template = document.getElementById("post-template")
-    searchResults.appendChild(htmlPostElement(getRandomPost(), template))
+    pictureEl.remove()
   }
+
+  let titleEl = newElement.querySelector("a.post-title")
+  titleEl.href = item.url
+  titleEl.text = item.title
+
+  newElement.querySelector("span.post-author").innerText = item.author
+  newElement.querySelector("span.post-date").innerText = item.date
+
+  let contentEl = newElement.querySelector("div.post-content")
+  contentEl.innerHTML = item.content
+
+  if (item.continue === 1) {
+    contentEl.appendChild(document.createElement("p")).innerHTML = "&#8230;"
+
+    let keepReadingEl = contentEl.appendChild(document.createElement("a"))
+    keepReadingEl.classList.add("subtitle", "is-5", "has-text-link")
+    keepReadingEl.href = item.url
+    keepReadingEl.text = "Seguir leyendo"
+  }
+
+  return newElement
+}
+
+document.addEventListener("turbolinks:load", () => {
+  document.addEventListener("keydown", (evt) => {
+    if (!isInput(evt.target.nodeName)) {
+      if (evt.key === "/") {
+        evt.preventDefault()
+        return showSearchModal()
+      }
+    }
+
+    if (evt.key === "Escape" || evt.key === "Esc") {
+      evt.preventDefault()
+      return hideSearchModal()
+    }
+  })
 })
