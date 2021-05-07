6a42716dc6d3e032cc52d0476b17bf540d3444f6
Halí V <halivelazquez@outlook.com>
2021-05-07 13:51:29 -0500

    Update search bar and active links

--- _resources/js/main.js
+++ _resources/js/main.js
@@ -1,127 +1,144 @@
 function addClass(element, name) {
-	if (!element) return
+  if (!element) return
 
-	if (element.classList) {
-		element.classList.add(name)
-	} else {
-		let arr = element.className.split(" ")
-		if (arr.indexOf(name) === -1) element.className += " " + name
-	}
+  if (element.classList) {
+    element.classList.add(name)
+  } else {
+    let arr = element.className.split(" ")
+    if (arr.indexOf(name) === -1) element.className += " " + name
+  }
 }
 
 function toggleClass(element, className) {
-	if (element.classList) {
-		element.classList.toggle(className)
-	} else {
-		let classes = element.className.split(" ")
-		let i = classes.indexOf(className)
+  if (element.classList) {
+    element.classList.toggle(className)
+  } else {
+    let classes = element.className.split(" ")
+    let i = classes.indexOf(className)
 
-		if (i >= 0) {
-			classes.splice(i, 1)
-		} else {
-			classes.push(className)
-		}
+    if (i >= 0) {
+      classes.splice(i, 1)
+    } else {
+      classes.push(className)
+    }
 
-		element.className = classes.join(" ")
-	}
+    element.className = classes.join(" ")
+  }
 }
 
 function hasClass(element, name) {
-	if (!element) return
+  if (!element) return
 
-	if (element.classList) {
-		return element.classList.contains(name)
-	} else {
-		let classes = element.className.split(" ")
-		let i = classes.indexOf(name)
-		return i >= 0
-	}
+  if (element.classList) {
+    return element.classList.contains(name)
+  } else {
+    let classes = element.className.split(" ")
+    let i = classes.indexOf(name)
+    return i >= 0
+  }
 }
 
 function removeClass(element, name) {
-	if (!element) return
+  if (!element) return
 
-	if (element.classList) {
-		element.classList.remove(name)
-	} else {
-		let classes = element.className.split(" ")
-		let i = classes.indexOf(className)
+  if (element.classList) {
+    element.classList.remove(name)
+  } else {
+    let classes = element.className.split(" ")
+    let i = classes.indexOf(className)
 
-		if (i >= 0) classes.splice(i, 1)
-		element.className = classes.join(" ")
-	}
+    if (i >= 0) classes.splice(i, 1)
+    element.className = classes.join(" ")
+  }
 }
 
 function isInput(name) {
-	let compare = name.toLowerCase()
-	return ["input", "textarea", "select"].includes(compare)
+  let compare = name.toLowerCase()
+  return ["input", "textarea", "select"].includes(compare)
 }
 
 function vibrate(pattern) {
-	window.navigator.vibrate(pattern)
+  window.navigator.vibrate(pattern)
 }
 
 function initThemeSwitcher(element) {
-	if (!element) return
+  if (!element) return
 
-	const firstSpan = element.getElementsByTagName("span")[0]
-	const icon = firstSpan.getElementsByTagName("i")[0]
-	const isDark = hasClass(document.documentElement, "dark")
+  const firstSpan = element.getElementsByTagName("span")[0]
+  const icon = firstSpan.getElementsByTagName("i")[0]
+  const isDark = hasClass(document.documentElement, "dark")
 
-	removeClass(icon, isDark ? "fa-moon" : "fa-sun")
-	addClass(icon, isDark ? "fa-sun" : "fa-moon")
-	removeClass(element, isDark ? "is-light" : "is-dark")
-	addClass(element, isDark ? "is-dark" : "is-light")
+  removeClass(icon, isDark ? "fa-moon" : "fa-sun")
+  addClass(icon, isDark ? "fa-sun" : "fa-moon")
+  removeClass(element, isDark ? "is-light" : "is-dark")
+  addClass(element, isDark ? "is-dark" : "is-light")
 
-	removeClass(element, "is-invisible")
+  removeClass(element, "is-invisible")
 }
 
 function setTheme() {
-	if (localStorage.halivertsTheme && localStorage.halivertsTheme === "light") {
-		removeClass(document.documentElement, "dark")
-		localStorage.halivertsTheme = "light"
-	} else {
-		localStorage.removeItem("halivertsTheme")
-	}
+  if (localStorage.halivertsTheme && localStorage.halivertsTheme === "light") {
+    removeClass(document.documentElement, "dark")
+    localStorage.halivertsTheme = "light"
+  } else {
+    localStorage.removeItem("halivertsTheme")
+  }
 }
 
-function hideModal(evt) {
-	if (!evt) return
+function setActive(menu) {
+  if (!menu) return
 
-	const modal = evt.closest(".modal")
-	if (!modal) return
+  const elements = menu.querySelectorAll("li > a")
 
-	addClass(modal, "is-hidden")
+  removeClass(menu.querySelector("li > .is-active"), "is-active")
+
+  elements.forEach((element) => {
+    if (window.location.href === element.href) {
+      addClass(element, "is-active")
+    }
+  })
+}
+
+function hideModal(target) {
+  if (!target) return
+
+  const modal = target.closest(".modal")
+  if (!modal) return
+
+  addClass(modal, "is-hidden")
+  setActive(document.getElementById("side-menu"))
 }
 
 document.addEventListener("turbo:load", () => {
-	initThemeSwitcher(document.getElementById("theme-switcher"))
-	initThemeSwitcher(document.getElementById("theme-switcher-corner"))
+  initThemeSwitcher(document.getElementById("theme-switcher"))
+  initThemeSwitcher(document.getElementById("theme-switcher-corner"))
 
-	const deletes = document.querySelectorAll(".notification .delete") || []
+  const deletes = document.querySelectorAll(".notification .delete") || []
 
-	deletes.forEach((deleteElement) => {
-		notification = deleteElement.parentNode
-		deleteElement.addEventListener("click", () => {
-			notification.parentNode.removeChild(notification)
-		})
-	})
+  deletes.forEach((deleteElement) => {
+    notification = deleteElement.parentNode
+    deleteElement.addEventListener("click", () => {
+      notification.parentNode.removeChild(notification)
+    })
+  })
 
-	const modals = document.querySelectorAll(".modal") || []
-	modals.forEach((modal) => {
-		modal.addEventListener("click", (evt) => {
-			if (evt.target !== modal) {
-				return
-			}
+  const modals = document.querySelectorAll(".modal") || []
+  modals.forEach((modal) => {
+    modal.addEventListener("click", (evt) => {
+      if (evt.target !== modal) {
+        return
+      }
 
-			evt.preventDefault()
-			toggleClass(modal, "is-hidden")
-		})
-	})
+      evt.preventDefault()
+      hideModal(evt.target)
+    })
+  })
+
+  setActive(document.getElementById("side-menu"))
 })
 
 document.addEventListener("turbo:visit", () => {
-	setTheme()
+  setTheme()
 })
 
 setTheme()
