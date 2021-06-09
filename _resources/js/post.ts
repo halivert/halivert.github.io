897b328ea8bd99d7d7b2e01c0c075667d36a922a
Halí V <halivelazquez@outlook.com>
2021-06-09 15:28:31 -0500

    Fix initial load

--- _resources/js/post.ts
+++ _resources/js/post.ts
@@ -1,102 +1,107 @@
 import { addClass } from "./functions"
 
 declare global {
-  interface Window {
-    getReactions: Function
-    mentionsUrl: string
-    postUrl: string
-  }
+	interface Window {
+		getReactions: Function
+		mentionsUrl: string
+		postUrl: string
+	}
 
-  interface Reactions {
-    count: number
-    type: Object
-  }
+	interface Reactions {
+		count: number
+		type: Object
+	}
 }
 
 const fixTwitterLink = () => {
-  const tweetLink: HTMLAnchorElement = document.querySelector(
-    "#reactions .twitter-share-button"
-  )
+	const tweetLink: HTMLAnchorElement = document.querySelector(
+		"#reactions .twitter-share-button"
+	)
 
-  if (!tweetLink) return
+	if (!tweetLink) return
 
-  const tweetUrl: URL = new URL("https://twitter.com/intent/tweet")
-  const postUrl: URL = new URL(window.postUrl)
+	const tweetUrl: URL = new URL("https://twitter.com/intent/tweet")
+	const postUrl: URL = new URL(window.postUrl)
 
-  const author: string = (<HTMLMetaElement>(
-    document.head.querySelector("meta[name='author']")
-  )).content
+	const author: string = (<HTMLMetaElement>(
+		document.head.querySelector("meta[name='author']")
+	)).content
 
-  const data = {
-    original_referer: `${postUrl.origin}`,
-    text: document.title,
-    url: postUrl.href,
-    via: author,
-  }
+	const data = {
+		original_referer: `${postUrl.origin}`,
+		text: document.title,
+		url: postUrl.href,
+		via: author,
+	}
 
-  Object.keys(data).forEach((key: string) => {
-    tweetUrl.searchParams.append(key, data[key])
-  })
+	Object.keys(data).forEach((key: string) => {
+		tweetUrl.searchParams.append(key, data[key])
+	})
 
-  tweetLink.href = tweetUrl.toString()
+	tweetLink.href = tweetUrl.toString()
 }
 
 const loadReactions = async () => {
-  const reactions: Reactions = await window.getReactions()
-  const reactionsSection: HTMLElement = document.getElementById("reactions")
+	const reactionsSection: HTMLElement = document.getElementById("reactions")
+	if (!reactionsSection || !window.getReactions) return
 
-  const availableReactions = {
-    like: {
-      iconClass: ["fas", "fa-star", "has-text-warning"],
-    },
-    repost: {
-      iconClass: ["fas", "fa-retweet", "has-text-success", "ml-3"],
-    },
-    reply: {
-      iconClass: ["fas", "fa-comment-dots", "has-text-text", "ml-3"],
-    },
-    mention: {
-      iconClass: ["fas", "fa-quote-right", "has-text-text", "ml-3"],
-      filter: "mention-of",
-    },
-  }
+	const reactions: Reactions = await window.getReactions()
 
-  const iconsDiv: HTMLDivElement = document.createElement("div")
-  addClass(iconsDiv, ["reactions"])
+	const availableReactions = {
+		like: {
+			iconClass: ["fas", "fa-star", "has-text-warning"],
+		},
+		repost: {
+			iconClass: ["fas", "fa-retweet", "has-text-success", "ml-3"],
+		},
+		reply: {
+			iconClass: ["fas", "fa-comment-dots", "has-text-text", "ml-3"],
+		},
+		mention: {
+			iconClass: ["fas", "fa-quote-right", "has-text-text", "ml-3"],
+			filter: "mention-of",
+		},
+	}
 
-  Object.keys(availableReactions).forEach((key: string) => {
-    const reactionCategory = availableReactions[key]
+	const iconsDiv: HTMLDivElement = document.createElement("div")
+	addClass(iconsDiv, ["reactions"])
 
-    const text: Text = document.createTextNode(reactions.type[key] || 0)
-    const icon: HTMLElement = document.createElement("i")
-    addClass(icon, [...reactionCategory.iconClass, "mr-2"])
+	Object.keys(availableReactions).forEach((key: string) => {
+		const reactionCategory = availableReactions[key]
 
-    if (reactionCategory.filter && reactions.type[key]) {
-      const link: HTMLAnchorElement = document.createElement("a")
-      link.href = `${window.mentionsUrl}&wm-property=${reactionCategory.filter}`
-      link.appendChild(icon)
-      link.appendChild(text)
-      iconsDiv.appendChild(link)
-    } else {
-      const span: HTMLSpanElement = document.createElement("span")
-      span.appendChild(icon)
-      span.appendChild(text)
-      iconsDiv.appendChild(span)
-    }
-  })
+		const text: Text = document.createTextNode(reactions.type[key] || 0)
+		const icon: HTMLElement = document.createElement("i")
+		addClass(icon, [...reactionCategory.iconClass, "mr-2"])
 
-  const flexDiv: HTMLDivElement = reactionsSection.querySelector("div.flex")
+		if (reactionCategory.filter && reactions.type[key]) {
+			const link: HTMLAnchorElement = document.createElement("a")
+			link.href = `${window.mentionsUrl}&wm-property=${reactionCategory.filter}`
+			link.appendChild(icon)
+			link.appendChild(text)
+			iconsDiv.appendChild(link)
+		} else {
+			const span: HTMLSpanElement = document.createElement("span")
+			span.appendChild(icon)
+			span.appendChild(text)
+			iconsDiv.appendChild(span)
+		}
+	})
 
-  const lastReactions: HTMLDivElement = flexDiv.querySelector("div.reactions")
+	const flexDiv: HTMLDivElement = reactionsSection.querySelector("div.flex")
 
-  if (lastReactions) {
-    flexDiv.removeChild(lastReactions)
-  }
+	const lastReactions: HTMLDivElement = flexDiv.querySelector("div.reactions")
 
-  flexDiv.appendChild(iconsDiv)
+	if (lastReactions) {
+		flexDiv.removeChild(lastReactions)
+	}
+
+	flexDiv.appendChild(iconsDiv)
 }
 
 document.addEventListener("turbo:load", () => {
-  fixTwitterLink()
-  loadReactions()
+	fixTwitterLink()
+	loadReactions()
 })
+
+fixTwitterLink()
+loadReactions()
