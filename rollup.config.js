4b61806682dab4d353550fd89b6529314df7fda6
Halí V <halivelazquez@outlook.com>
2021-07-26 15:08:04 -0500

    Execute some code before dom content loaded

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,23 @@
 import { defineConfig } from "rollup"
 import typescript from "@rollup/plugin-typescript"
 import { terser } from "rollup-plugin-terser"
-// import nodeResolve from "@rollup/plugin-node-resolve"
 
 export default (args) => {
-	return defineConfig([
-		{
-			input: [
-				"src/js/main.ts",
-				"src/js/post.ts",
-				"src/js/search.ts",
-				"src/js/themeSwitcher.ts",
-			],
-			output: {
-				dir: "assets/js/",
-				format: "esm",
-			},
-			plugins: [typescript(), args.configDev ? "" : terser()],
-		},
-	])
+  return defineConfig([
+    {
+      input: ["src/js/post.ts", "src/js/search.ts", "src/js/themeSwitcher.ts"],
+      output: {
+        dir: "assets/js/",
+        format: "esm",
+      },
+      plugins: [typescript(), args.configDev ? "" : terser()],
+    },
+    {
+      input: "src/js/main.ts",
+      output: {
+        dir: "assets/js",
+      },
+      plugins: [typescript(), args.configDev ? "" : terser()],
+    },
+  ])
 }
