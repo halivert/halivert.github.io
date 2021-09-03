7b32f4febce69339299d3e77f328314ad65e1778
Halí V <halivelazquez@outlook.com>
2021-09-03 11:24:19 -0500

    Finish post reactions

--- src/ts/post.ts
+++ src/ts/post.ts
@@ -1,11 +1,7 @@
 import { createApp, reactive } from "petite-vue"
-import { addClass, removeClass } from "./functions"
 
 declare global {
   interface Window {
-    getReactions: Function
-    mentionsUrl: string
-    postUrl: string
     Turbo: object
   }
 
@@ -14,8 +10,8 @@ declare global {
     type: Object
   }
 
-  interface ReactionsObject {
-    postUrl?: string
+  interface ReactionsProps {
+    postUrl: string
   }
 }
 
@@ -41,117 +37,80 @@ const twitterLink = (rawPostUrl: string): string => {
   return tweetUrl.toString()
 }
 
-const loadReactions = async () => {
-  const reactionsSection: HTMLElement = document.getElementById("reactions")
-  if (!reactionsSection || !window.getReactions) return
-
-  const reactions: Reactions = await window.getReactions()
+function Reactions(props: ReactionsProps) {
+  const apiUrl = "https://webmention.io/api"
 
   const availableReactions = {
     like: {
-      iconClass: ["fas", "fa-star", "has-text-warning"],
+      className: ["fa-star", "has-text-warning"],
     },
     repost: {
-      iconClass: ["fas", "fa-retweet", "has-text-success", "ml-3"],
+      className: ["fa-retweet", "has-text-success"],
     },
     reply: {
-      iconClass: ["fas", "fa-comment-dots", "has-text-text", "ml-3"],
+      className: ["fa-comment-dots", "has-text-text"],
     },
     mention: {
-      iconClass: ["fas", "fa-quote-right", "has-text-text", "ml-3"],
+      className: ["fa-quote-right", "has-text-text"],
       filter: "mention-of",
     },
   }
 
-  const iconsDiv: HTMLDivElement = document.createElement("div")
-  addClass(iconsDiv, ["reactions"])
+  async function fetchReactions() {
+    const fetchedReactions: Reactions = await (
+      await fetch(`${apiUrl}/count.json?target=${props?.postUrl}`)
+    ).json()
 
-  Object.keys(availableReactions).forEach((key: string) => {
-    const reactionCategory = availableReactions[key]
+    this.reactions = Object.entries(availableReactions).map(
+      ([key, value]: [string, object]) => {
+        const count: number = fetchedReactions.type?.[key] || 0
 
-    const text: Text = document.createTextNode(reactions.type[key] || 0)
-    const icon: HTMLElement = document.createElement("i")
-    addClass(icon, [...reactionCategory.iconClass, "mr-2"])
+        if (value?.["filter"] && count) {
+          this.mentions.count = count
+          return null
+        }
 
-    if (reactionCategory.filter && reactions.type[key]) {
-      const link: HTMLAnchorElement = document.createElement("a")
-      link.href = `${window.mentionsUrl}&wm-property=${reactionCategory.filter}`
-      link.appendChild(icon)
-      link.appendChild(text)
-      iconsDiv.appendChild(link)
-    } else {
-      const span: HTMLSpanElement = document.createElement("span")
-      span.appendChild(icon)
-      span.appendChild(text)
-      iconsDiv.appendChild(span)
-    }
-  })
+        return {
+          count: count,
+          name: key,
+          className: [...value["className"], "fa", "ml-3", "mr-2"],
+        }
+      }
+    )
 
-  const flexDiv: HTMLDivElement = reactionsSection.querySelector("div.flex")
-
-  const lastReactions: HTMLDivElement = flexDiv.querySelector("div.reactions")
-
-  if (lastReactions) {
-    flexDiv.removeChild(lastReactions)
+    if (!fetchedReactions.count) this.reactions.reverse()
   }
 
-  flexDiv.appendChild(iconsDiv)
+  function getMentionsUrl(props: Object) {
+    const mentionsUrl = new URL(`${props?.["apiUrl"]}/mentions.html`)
+
+    const queryParameters = {
+      target: props?.["postUrl"],
+      "wm-property": props?.["filter"],
+    }
+
+    Object.entries(queryParameters).forEach(([key, value]) => {
+      mentionsUrl.searchParams.append(key, value)
+    })
+
+    return mentionsUrl.toString()
+  }
+
+  return {
+    reactions: [],
+    mentions: {
+      url: getMentionsUrl({
+        apiUrl,
+        postUrl: props?.postUrl,
+        filter: availableReactions.mention.filter,
+      }),
+    },
+
+    fetchReactions,
+  }
 }
 
 const mountApp = () => {
-  function Reactions(props: ReactionsObject) {
-    const apiUrl = "https://webmention.io/api"
-    const reactions = reactive({ value: [] })
-
-    const availableReactions = {
-      like: {
-        className: ["fa-star", "has-text-warning"],
-      },
-      repost: {
-        className: ["fa-retweet", "has-text-success"],
-      },
-      reply: {
-        className: ["fa-comment-dots", "has-text-text"],
-      },
-      mention: {
-        className: ["fa-quote-right", "has-text-text"],
-        filter: "mention-of",
-      },
-    }
-
-    const mentions = reactive({
-      url: `${apiUrl}/mentions.html?target=${props?.postUrl}&wm-property=${availableReactions.mention.filter}`,
-    })
-    ;(async () => {
-      const fetchedReactions: Reactions = await (
-        await fetch(`${apiUrl}/count.json?target=${props?.postUrl}`)
-      ).json()
-
-      Object.entries(availableReactions).forEach(
-        ([key, value]: [string, object]) => {
-          const count: number = fetchedReactions.type?.[key] || 0
-          value["name"] = key
-          value["className"].push("fa", "ml-3", "mr-2")
-          value["count"] = count
-
-          if (value?.["filter"] && count) {
-            mentions.count = count
-            return
-          }
-
-          reactions.value.push(value)
-        }
-      )
-
-      if (!fetchedReactions.count) reactions.value.reverse()
-    })()
-
-    return {
-      reactions: reactions.value,
-      mentions,
-    }
-  }
-
   createApp({
     twitterLink,
     Reactions,
