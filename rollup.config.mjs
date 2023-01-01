f714c3e1a5b984f3fbe406c0232daf39767b46c9
halivert <halivelazquez+github@gmail.com>
2023-01-01 17:10:53 +0000

    Deploying to gh-pages from @ halivert/halivert.github.io@ac03a7f5f62678f0e07cb7fd6447210044e70799 🚀

---
+++ rollup.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from "rollup"
+
+import typescript from "@rollup/plugin-typescript"
+import terser from "@rollup/plugin-terser"
+import alias from "@rollup/plugin-alias"
+import nodeResolve from "@rollup/plugin-node-resolve"
+import commonJS from "@rollup/plugin-commonjs"
+
+console.log()
+
+export default (args) => {
+  const plugins = [
+    alias({
+      entries: {
+        "petite-vue": "petite-vue/dist/petite-vue.es.js",
+      },
+    }),
+    typescript(),
+    nodeResolve(),
+    commonJS(),
+    args.configDev ? "" : terser(),
+  ]
+
+  return defineConfig([
+    {
+      input: ["src/ts/main.ts", "src/ts/post.ts", "src/ts/search.ts"],
+      output: {
+        dir: "assets/js/",
+        format: "esm",
+        sourcemap: args.configDev,
+      },
+      plugins: plugins,
+    },
+  ])
+}
