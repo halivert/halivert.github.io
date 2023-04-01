44531997f23562dd22dc580af0f43b9a7ae092d8
Halí V <halivelazquez@outlook.com>
2023-03-31 22:35:42 -0600

    feat: update files

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'dotenv'
 gem 'jekyll', '~> 4.3'
 
 group :jekyll_plugins do
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,17 +2,11 @@ import { defineConfig } from "rollup"
 
 import typescript from "@rollup/plugin-typescript"
 import terser from "@rollup/plugin-terser"
-import alias from "@rollup/plugin-alias"
 import nodeResolve from "@rollup/plugin-node-resolve"
 import commonJS from "@rollup/plugin-commonjs"
 
 export default (args) => {
   const plugins = [
-    alias({
-      entries: {
-        "petite-vue": "petite-vue/dist/petite-vue.es.js",
-      },
-    }),
     typescript(),
     nodeResolve(),
     commonJS(),
