a97cb9be3d442d43fa361c81d210bd1d98825dca
Halí V. <halivelazquez@outlook.com>
2024-12-14 13:47:58 -0600

    feat: prepare for deploy

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://halivert.dev',
   redirects: {
     "/blog": "/blog/1",
     "/en/blog": "/en/blog/1",
